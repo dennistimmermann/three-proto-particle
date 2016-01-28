f25c847d455f050194b7e76beaa4c6193f025b3c
Dennis Timmermann <timmermann.dennis@googlemail.com>
2016-01-28 16:48:57 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,73 @@
+# Proto Particle
+
+Proto Particle is a really simple "particle system"
+It doesn't include any physics whatsoever, that up to you.
+It's basically just a system to efficiently render a ton of particles/geometries in Three JS.
+This code may also be a good start to experiment with Three JS BufferGeometries / InstancedBufferGeometries
+
+### Features
+- all particles are billboarded
+- spritemap support
+- use any geometry as particle
+- batched updates
+
+### coming soon to a particle near you
+- respecting normals
+- z-sorting
+- examples
+
+## how to particle
+
+### Creating the particle system
+
+```
+var Particles = require('Proto-Particles')
+
+/* in it's easiest form with 1024 particles*/
+var particles = new Particle.System()
+
+/* with a custom number of particles */
+var particles = new Particle.System(64)
+
+/* with material changes, eg a texture */
+var particles = new Particle.System(64, new Particles.Material( { 'texture': THREE.ImageUtils.loadTexture( "images/uv.jpg" )} )
+
+/* use a spritsheet with 4 columns and 2 rows */
+var particles = new Particle.System(64, new Particles.Material( { 'texture': THREE.ImageUtils.loadTexture( "images/sprites.jpg" )} , 4, 2)
+
+/* just using a custom geometry */
+var particles = new Particle.System(4096, new Particles.Material() , 1, 1, new THREE.CircleBufferGeometry(1,6))
+```
+
+### Adding / manipulating particles
+
+```
+/* just setting the position of a particle */
+particles.addParticle( new THREE.Vector3( 1, 1, 1 ) )
+particles.addParticle( [1, 1, 1 ] )
+
+/* setting position and custom attributes */
+particles.addParticle( [1, 1, 1 ], { size: 2, opacity: 0.5, color: [ 0.5, 0.5, 1 ], sprite: 2 } )
+
+/* just setting single attributes (translation and size) of a single particle (particle #45) */
+particles.setAttribute( 'translate' , 45, [2, 0, 5] )
+particles.setAttribute( 'size' , 45, 2 )
+
+/* setting an attribute (translation) for a bunch of particles */
+var translations = particles.getAttributeArray('translation')
+var sizes = particles.getAttributeArray('size')
+for( var i = 0; i < particles.particleCount; i++ ) {
+    /* keep in mind, that different attributes consist of different number of attributes, 
+     * e.g. x,y,z for translation and only s for size */
+     translations[ i*3 + 0 ] = Math.random() // x
+     translations[ i*3 + 1 ] = Math.random() // y
+     translations[ i*3 + 2 ] = Math.random() // z
+
+     sizes[ i ] = Math.random()
+}
+```
+
+### ... more will come soon
+
+
+
---
+++ proto-particle.js
@@ -0,0 +1,255 @@
+/**
+ * Simple Particle System
+ * @author Dennis Timmermann
+ *
+ * TODO:
+ * 	add z-sorting
+ *  respect normals
+ */
+
+/* require */
+var THREE = require('three')
+
+/**
+ * p AR ticle system
+ *
+ * @class  Particles
+ */
+class Particles extends THREE.Object3D {
+	constructor( num_particles = 1024, material = new Material(), columns = 1, rows = 1, tmpl = new THREE.PlaneBufferGeometry( 1, 1, 1, 1 ) ) {
+		super( )
+
+		/* trust me, we dont want more than 65k particles */
+		if( num_particles > 0xffff ) {
+			throw new Error( 'too many particles' )
+		}
+
+		this.num_particles = num_particles
+		/* keep track of wich particles we already used */
+		this.iter = 0
+
+		/* keept track of wich attributes we have to update for the next frame */
+		this.dirty_attributes = new Set()
+
+		/* setting up the instanced geometry. */
+		var geometry = new THREE.InstancedBufferGeometry()
+
+		/* get the meat from the geometry */ //TODO: do something with normals
+		var positions = new Float32Array(tmpl.getAttribute('position').array)
+		var indices = new Uint32Array(tmpl.getIndex().array)
+		var uvs = new Float32Array(tmpl.getAttribute('uv').array)
+
+		/* check if we want to use tiled spritesheets */
+		if(rows > 1 || columns > 1) {
+			/* prepare the uvs */
+			for( var i2 = 0; i2 < uvs.length; i2 += 2) {
+				uvs[ i2 + 0 ] /= columns
+				uvs[ i2 + 1 ] /= rows
+			}
+			/* and the positions */
+			for( var i3 = 0; i3 < positions.length; i3 += 3 ) {
+				positions[ i3 + 0 ] *= ( rows / columns )
+			}
+			// recalculationg normals will be harder i guess?
+		}
+		/* supply the material to our spritemap */
+		material.uniforms.spritemap = { type: "v2", value: new THREE.Vector2(columns, rows) }
+
+		/* setting up custom attributes. You can add your own and use them in the vertex shader ...
+		 * these attributes are custom for every particle, so be ressourceful :D
+		 * where particles will be translated to ...  * 3 components ( x, y, z ) */
+		var translations = new Float32Array( num_particles * 3 )
+		/* wich sprite to use ...  * 2 components ( column, row )
+		 * 0..column * row, starting bottom left going to right, growing upwards */
+		var sprites = new Float32Array( num_particles )
+		/* how big the particle will be ...  * 1 component (size)
+		 * 0... */
+		var sizes = new Float32Array( num_particles )
+		/* how visible the particle will be ...  * 1 component (opacity)
+		 * 0..1 */
+		var opacities = new Float32Array( num_particles )
+		/* what color to color the particle ...  * 1 component (r, g, b)0..1 */
+		var colors = new Float32Array(num_particles * 3)
+		/* add your own here ... */
+
+		/* typed arrays are initialized with 0, if you want other values, do that here */
+		for ( var i = 0, i2 = 0, i3 = 0; i < num_particles; i++, i2 += 2, i3 += 3 ) {
+			colors[ i3 + 0 ] = 1.0
+			colors[ i3 + 1 ] = 1.0
+			colors[ i3 + 2 ] = 1.0
+
+			opacities[ i ] = 1.0
+			//...
+		}
+
+		/* add attributes to geometry
+		 * these attributes are shared for all particles */
+		geometry.setIndex( new THREE.BufferAttribute( indices, 1 )  )
+		geometry.addAttribute( 'position', new THREE.BufferAttribute( positions, 3 ,1 ) )
+		geometry.addAttribute( 'uv', new THREE.BufferAttribute( uvs, 2 , 1) )
+
+		/* custom for every particle */
+		geometry.addAttribute( 'sprite', new THREE.InstancedBufferAttribute( sprites, 1 ,1 ) )
+		geometry.addAttribute( 'size', new THREE.InstancedBufferAttribute( sizes, 1, 1) )
+		geometry.addAttribute( 'translate', new THREE.InstancedBufferAttribute( translations, 3, 1 ) )
+		geometry.addAttribute( 'color', new THREE.InstancedBufferAttribute( colors, 3, 1 ) )
+		geometry.addAttribute( 'opacity', new THREE.InstancedBufferAttribute( opacities, 1 ,1 ) )
+
+		/* create mesh and add it */
+		this.mesh = new THREE.Mesh(geometry, material)
+		this.add(this.mesh)
+
+		/* calculating a bounding box can be quite expensive so we just render the particles no matter what */
+		this.frustumCulled = false
+		this.mesh.frustumCulled = false
+
+		/* sorting stuff */ // ... will fix
+		// this.sort = {
+		// 	distance_array: new Float32Array(num_particles)
+		// }
+	}
+
+	/**
+	 * add particles. The only necessary argument is the position.
+	 * through otions you can specify the other attributes
+	 *
+	 * @param {Array | THREE.Vector3} translate the position of the particle
+	 * @param {Object} options options array containing settings for attributes
+	 */
+	addParticle(translate, options = {}) {
+		/* if the position is a Vector, convert it to an array */
+		if( translate.toArray ) translate = translate.toArray()
+		options.translate = translate
+
+		/* by default we set size to 1*/
+		if( options.size == undefined ) options.size = 1.0
+
+		/* set all the attributes */
+		for( var prop in options ) {
+			this.setAttribute(prop, this.iter, options[prop])
+
+		}
+
+		this.iter = ( this.iter+1 ) % this.num_particles
+	}
+
+	/* more low level, use this if you know what you are doing */
+	/**
+	 * returns the raw typed array for the attribute. Dirty flag will be set for you
+	 *
+	 * @param {String} name attribute name
+	 * @return {typed Array} the raw attribute array
+	 */
+	getAttributeArray(name) {
+		this.dirty_attributes.add(name)
+		return this.mesh.geometry.getAttribute(name).array
+	}
+
+	/**
+	 * set single namend attribute at single position. The attribute width will be infered from the amount of arguments supplied
+	 *
+	 * @param {String} name Attribute name
+	 * @param {Integer} iter which particle, 0..num_particles
+	 * @param {Number|Array of Numbers} values attribute values
+	 */
+	setAttribute(name, iter, values) {
+		if(!(values instanceof Array)) values = [values]
+
+		var attribute = this.mesh.geometry.getAttribute(name)
+		var offset = iter * values.length
+
+		for( var i = 0; i < values.length; i ++ ) {
+			attribute.array[ offset + i ] = values[i]
+		}
+
+		this.dirty_attributes.add(name)
+	}
+
+	/**
+	 * call this every time you made changes to the particles or every frame
+	 */
+	update() {
+		/* update all dirt attributes */
+		this.dirty_attributes.forEach((attribute) => {
+			this.mesh.geometry.getAttribute(attribute).needsUpdate = true
+		})
+		this.dirty_attributes.clear()
+	}
+}
+
+/**
+ * Material for the Particle System
+ *
+ * @class Material
+ */
+class Material extends THREE.ShaderMaterial {
+	constructor(options) {
+
+		/* default options for the material, basically just the shaders */
+		var def = {
+			uniforms: {
+				texture:   { type: "t", value: undefined },
+			},
+			vertexShader:   vertexShader,
+			fragmentShader: fragmentShader,
+		}
+
+		/* custom options can be passed and will be applied to the defaults */
+		for( var prop in options ) {
+			/* uniforms will be added */
+			if( prop == 'uniforms' ) {
+				for( var uniform in options.uniforms ) {
+					def.uniforms[uniform] = options.uniform[uniform]
+				}
+			}
+			/* texture is a special case and will be added to uniforms */
+			else if( prop == 'texture' ) {
+				def.uniforms[prop].value = options[prop]
+			}
+			/* add the rest */
+			else def[prop] = options[prop]
+		}
+		super(def)
+	}
+}
+
+var vertexShader = `
+	attribute float size;
+	attribute vec3 translate;
+	attribute vec2 sprite;
+	attribute float opacity;
+	attribute vec3 color;
+
+	varying float vOpacity;
+	varying vec3 vColor;
+	varying vec2 vUv;
+
+	void main() {
+
+		vColor = color;
+		vOpacity = opacity;
+		vUv = uv;
+
+		vec4 mvPosition = modelViewMatrix * vec4( translate, 1.0 );
+		mvPosition.xyz += position * size;
+
+		gl_Position = projectionMatrix * mvPosition;
+	}
+`
+
+var fragmentShader = `
+	uniform sampler2D texture;
+	varying vec2 vUv;
+	varying vec3 vColor;
+	varying float vOpacity;
+	void main() {
+		gl_FragColor = vec4( vColor, vOpacity ) * texture2D( texture, vUv);
+	}
+`
+
+module.exports = {
+	System: Particles,
+	Material: Material,
+	fragmentShader: fragmentShader,
+	vertexShader: vertexShader
+}
